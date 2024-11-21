a641c394a5c013471f1ab14ceae0e29ae8480659
neakovalainen <neakovalaine@gmail.com>
2024-11-21 17:32:12 +0200

    Refactored reference_creation and validate_inproceeding

--- src/app.py
+++ src/app.py
@@ -20,51 +20,29 @@ def new():
 
 @app.route("/create_reference", methods=["POST"])
 def reference_creation():
+    # mandatory fields
     reference_id = request.form.get("reference_id")
     author = request.form.get("author")
     title = request.form.get("title")
     booktitle = request.form.get("booktitle")
     year = request.form.get("year")
 
-    editor = request.form.get("editor")
-    editor = None if editor == "" else editor
-    volume = request.form.get("volume")
-    volume = None if volume == "" else volume
-    number = request.form.get("number")
-    number = None if number == "" else number
-    series = request.form.get("series")
-    series = None if series == "" else series
-    pages = request.form.get("pages")
-    pages = None if pages == "" else pages
-    address = request.form.get("address")
-    address = None if address == "" else address
-    month = request.form.get("month")
-    month = None if month == "" else month
-    organization = request.form.get("organization")
-    organization = None if organization == "" else organization
-    publisher = request.form.get("publisher")
-    publisher = None if publisher == "" else publisher
+    # optional fields
+    editor = request.form.get("editor") or None
+    volume = request.form.get("volume") or None
+    number = request.form.get("number") or None
+    series = request.form.get("series") or None
+    pages = request.form.get("pages") or None
+    address = request.form.get("address") or None
+    month = request.form.get("month") or None
+    organization = request.form.get("organization") or None
+    publisher = request.form.get("publisher") or None
 
     try:
         # Inputs given as arguments to the validation function //found in util.py
         validate_inproceeding(reference_id, author, title, booktitle, year, editor, volume, number, series, pages, address, month, organization, publisher)
-        create_inproceeding(
-            reference_id=reference_id,
-            author=author,
-            title=title,
-            booktitle=booktitle,
-            year=year,
-            editor=editor,
-            volume=volume,
-            number=number,
-            series=series,
-            pages=pages,
-            address=address,
-            month=month,
-            organization=organization,
-            publisher=publisher,
-        )
-        flash(str("Added succesfully"))
+        create_inproceeding(reference_id, author, title, booktitle, year, editor, volume, number, series, pages, address, month, organization, publisher)
+        flash("Added succesfully")
         return redirect("/")
     except Exception as error:
         flash(str(error))
