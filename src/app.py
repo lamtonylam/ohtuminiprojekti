bee41335d97fce8a362c517a216219d6be071bfd
ebadihamed <ebadihamed18@gmail.com>
2024-11-21 19:43:38 +0200

    changes made to UI

--- src/app.py
+++ src/app.py
@@ -17,6 +17,10 @@ def index():
 def new():
     return render_template("new_reference.html")
 
+@app.route("/preview")
+def preview():
+    return render_template("preview.html")
+
 
 @app.route("/create_reference", methods=["POST"])
 def reference_creation():
@@ -47,6 +51,13 @@ def reference_creation():
     except Exception as error:
         flash(str(error))
         return redirect("/new_reference")
+
+
+@app.route("/download", methods=["POST"])
+def download():
+    flash("Function is not ready")
+    return redirect("/")
+
 
 
 # testausta varten oleva reitti
