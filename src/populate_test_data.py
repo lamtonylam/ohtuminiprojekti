faf10b18dc26938f513d71ef1b8658e6db2c1991
OGesko <eskelinenvilkko@gmail.com>
2024-12-02 15:03:42 +0200

    fixed test data creation
    Co-authored-by: Hamed <ebadihamed18@gmail.com>

--- src/populate_test_data.py
+++ src/populate_test_data.py
@@ -2,6 +2,7 @@ from config import db
 from test_data import test_data
 from sqlalchemy import text
 from validate import validate_article, validate_book, validate_inproceedings
+from repositories.references_repository import create_reference
 
 table_name = "reference"
 
@@ -9,16 +10,14 @@ table_name = "reference"
 def populate_database():
     sql = text(f"""
             INSERT INTO {table_name} (
-                reference_id, author, title, booktitle, year, editor, volume, number, 
-                series, pages, address, month, organization, publisher
-            )
+                reference_id, reference_type, title, author, publisher,
+                address, journal, volume, number, pages, month, note, booktitle, editor, series, organisation)
             VALUES (
-                :reference_id, :author, :title, :booktitle, :year, :editor, :volume, :number, 
-                :series, :pages, :address, :month, :organization, :publisher
+                :reference_id, :reference_type, :title, :author, :publisher,
+                :address, :journal, :volume, :number, :pages, :month, :note, :booktitle, :editor, :series, :organisation)
             )
             """)
 
-    for row in test_data:
-        db.session.execute(sql, row)
 
-    db.session.commit()
+    for reference in test_data: 
+        create_reference( reference_id=reference["reference_id"], reference_type=reference["reference_type"], title=reference["title"], author=reference["author"], year=reference["year"], booktitle=reference.get("booktitle"), editor=reference.get("editor"), volume=reference.get("volume"), number=reference.get("number"), series=reference.get("series"), pages=reference.get("pages"), address=reference.get("address"), journal=reference.get("journal"), month=reference.get("month"), note=reference.get("note"), organization=reference.get("organization"), publisher=reference.get("publisher"), )
