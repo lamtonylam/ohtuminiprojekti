33d0d26c7062d0fc1a1d8663843e49053ea66f55
neakovalainen <neakovalaine@gmail.com>
2024-12-10 14:58:58 +0200

    Added and modified test data

--- src/test_data.py
+++ src/test_data.py
@@ -1,7 +1,7 @@
 
 test_data = [
     {
-        "reference_id": "Adv2024",
+        "reference_id": "ref1",
         "reference_type": "inproceedings",
         "title": "Advances in AI",
         "author": "Alice Johnson",
@@ -11,21 +11,53 @@ test_data = [
         "organization": "AI Society"
     },
     {
-        "reference_id": "Dee2023",
+        "reference_id": "ref2",
         "reference_type": "article",
         "title": "Deep Learning Insights",
         "author": "Bob Smith",
         "year": 2023,
-        "publisher": "ML Publishers",
-        "volume": 12,
-        "number": 3
+        "journal": "ML Publishers",
+        "volume": "12",
+        "number": "3"
     },
     {
-        "reference_id": "Dat2022",
+        "reference_id": "ref3",
         "reference_type": "book",
         "title": "Data Science Fundamentals",
         "author": "Clara White",
         "year": 2022,
         "publisher": "Tech Books"
+    },
+    {
+        "reference_id": "ref4",
+        "reference_type": "book",
+        "title": "C language algorithms for digital signal processing",
+        "author": "Paul M. Embree, Bruce Kimble",
+        "year": 1991,
+        "publisher": "Prentice-Hall, Inc. United States"
+    },
+    {
+        "reference_id": "ref5",
+        "reference_type": "inproceedings",
+        "title": "Probability estimation by an adapted genetic algorithm in web insurance",
+        "author": "A.-L. Bedenel, L. Jourdan, C. Biernacki",
+        "year": 2019,
+        "booktitle": "Learning and intelligent optimization",
+        "editor": "R. Battiti, M. Brunato, I. Kotsireas, P. Pardalos",
+        "series": "Lecture Notes in Computer Science",
+        "volume": "11353",
+        "pages": "225-240",
+        "publisher": "Springer"
+    },
+    {
+        "reference_id": "ref6",
+        "reference_type": "article",
+        "title": "Machine learning for active matter",
+        "author": "F. Cichos, K. Gustavsson, B. Mehling, G. Volpe",
+        "year": 2020,
+        "journal": "nature",
+        "volume": "2",
+        "pages": "94–103"
     }
+
 ]
